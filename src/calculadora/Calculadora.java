79e3e84c9b33ef47794d23a705b401a14a9ca60f
davidasensi <davidinhomail@gmail.com>
2015-01-12 19:34:57 +0100

    Update Calculadora.java

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -47,15 +47,13 @@ public class Calculadora {
           System.out.println("El resultado de la resta es: "+c);
           break;
           case 3:
-           c=a*b;
+          c=a*b;
            System.out.println("El resultado de la multiplicacion es: "+c);
            break;
            case 4:
             c=a/b;
             System.out.println("El resultado de la multiplicacion es: "+c);
             break;
-            
-         
         }//fin switch
         
     }//fin main
